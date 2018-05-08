f94d80f83366eaf9a8ee3157c577ee43cd234f26
gnois <weishng@hotmail.com>
2018-05-08 15:12:30 +0800

    consolidate warn and compile, now that we no longer dump ast

--- lua/compile.lua
+++ lua/compile.lua
@@ -4,25 +4,51 @@
 
 
 local read = require("lua.read")
-local warn = require("lua.warn")
 local lex = require("lua.lex")
 local scope = require("lua.scope")
 local parse = require("lua.parse")
 local check = require("lua.check")
 local transform = require("lua.transform")
 local generate = require("lua.generate")
-local compile = function(reader, options, color)
-    local lexer = lex(reader, warn.add)
-    local tree = parse(lexer, warn.add)
-    tree = transform(tree)
-    local sc = scope(options.declares, warn.add)
-    check(sc, tree, warn.add)
-    for _, w in ipairs(warn.warnings) do
-        if w.severity >= 10 then
-            return nil, warn.format(color)
+local warnings = {}
+local warn = function(line, col, severity, msg)
+    local w = {line = line, col = col, severity = severity, msg = msg}
+    for i, m in ipairs(warnings) do
+        if line == m.line and severity < m.severity then
+            return 
+        end
+        if line < m.line or line == m.line and col < m.col then
+            table.insert(warnings, i, w)
+            return 
         end
     end
-    return generate(tree), warn.format(color)
+    table.insert(warnings, w)
+end
+local warning = function(color)
+    local warns = {}
+    for i, m in ipairs(warnings) do
+        local clr = color.yellow
+        if m.severity >= 10 then
+            clr = color.red
+        end
+        warns[i] = string.format(" %d,%d:" .. clr .. "  %s" .. color.reset, m.line, m.col, m.msg)
+    end
+    if #warns > 0 then
+        return table.concat(warns, "\n")
+    end
+end
+local compile = function(reader, options, color)
+    local lexer = lex(reader, warn)
+    local tree = parse(lexer, warn)
+    tree = transform(tree)
+    local sc = scope(options.declares, warn)
+    check(sc, tree, warn)
+    for _, w in ipairs(warnings) do
+        if w.severity >= 10 then
+            return nil, warning(color)
+        end
+    end
+    return generate(tree), warning(color)
 end
 return {string = function(src, options, color)
     return compile(read.string(src), options or {}, color)
